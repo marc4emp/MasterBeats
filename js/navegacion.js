f5614c95a7dd135e6a237d2ff6a3eee91c3f7beb
Marco <carrasco_marco@outlook.com>
2019-11-19 20:05:36 -0300

    link y otros

--- js/navegacion.js
+++ js/navegacion.js
@@ -9,10 +9,14 @@ function playSong(id) {
     if (cancion.classList.contains('pausa')) {
         cancion.classList.remove('pausa');
         cancion.className += " reprod";
+        cancion.parentNode.classList.remove('pausa');
+        cancion.parentNode.className += " reprod";
         cancion.play()
     } else {
         cancion.classList.remove('reprod');
         cancion.className += " pausa";
+        cancion.parentNode.classList.remove('reprod');
+        cancion.parentNode.className += " pausa";
         cancion.pause()
     }
 
